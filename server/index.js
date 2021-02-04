655876c758177c6e0dbee3a3538e7330f2081571
Tomiwa Obanla <obanlatomiwa@gmail.com>
2021-02-04 19:09:15 +0100

    added express routes

--- server/index.js
+++ server/index.js
@@ -29,3 +29,47 @@ pgClient
   .catch((err) => {
     console.log(err);
   });
+
+// Redis
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+
+  // if connect is lost, retry after 1s
+  retry_strategy: () => 1000,
+});
+
+const redisPublisher = redisClient.duplicate();
+
+//   Express routes
+app.get("/", (req, res) => {
+  res.send("Welcome to the Fibonacci Calculator");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * FROM values");
+  res.send(values);
+});
+
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post("/values", (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index too high");
+  }
+
+  redisClient.hset("values", index, "nothing yet");
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+  res.send({ working: true });
+});
+
+app.listen(8000, () => {
+  console.log("Listening to port 8000");
+});
