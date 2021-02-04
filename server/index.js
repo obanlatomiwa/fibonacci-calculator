2d227f3aecd3057209be8d3b1ca25dc0f016b23f
Tomiwa Obanla <obanlatomiwa@gmail.com>
2021-02-04 18:28:13 +0100

    added DB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
-const express = require('express');
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const keys = require('./keys')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const keys = require("./keys");
 
 // Express
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
+
+// Postgres DB
+const { Pool } = require("pg");
+
+const pgClient = new Pool({
+  user: keys.pgUser,
+  database: keys.pgDatabase,
+  host: keys.pgHost,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+});
+
+pgClient.on("error", () => {
+  console.log("Cannot connect to DB");
+});
+
+pgClient
+  .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+  .catch((err) => {
+    console.log(err);
+  });
